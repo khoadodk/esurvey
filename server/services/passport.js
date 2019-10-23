9bca4e42581c438c51771cddbd5244a0f9b443a4
Khoa Do <khoado.dk@gmail.com>
2019-10-23 15:32:38 -0400

    minor change to fix heroku issue

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,7 +1,6 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const mongoose = require('mongoose');
-require('dotenv').config();
 
 const User = mongoose.model('users');
 
